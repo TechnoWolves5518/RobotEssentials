6c0062b91906bd3d80b2d1b71bf013829b256683
Hamzah Aslam <haz540@gmail.com>
2017-01-24 23:35:04 -0500

    Updated ultrasonic distance methods --- still need to fix one with thread (won't work)

--- RobotUtils/src/org/usfirst/frc/team5518/utils/RobotUtils.java
+++ RobotUtils/src/org/usfirst/frc/team5518/utils/RobotUtils.java
@@ -1,5 +1,8 @@
 package org.usfirst.frc.team5518.utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import edu.wpi.first.wpilibj.Ultrasonic;
 
 /**
@@ -13,32 +16,36 @@ import edu.wpi.first.wpilibj.Ultrasonic;
 public class RobotUtils 
 {	
 	/**
+	 * Find the average distance from
+	 * an ultrasonic/proxmity sensor.
 	 * 
-	 * @param sensor
-	 * @return
+	 * @param sensor Ultrasonic object
+	 * @return Average distance
 	 */
 	public static double avgDistance(Ultrasonic sensor) {
-		return avgDistance(sensor, 1000);
+		return avgDistance(sensor, 1000, 10);
 	}
 	
 	/**
+	 * Find the average distance from
+	 * an ultrasonic/proximity sensor.
 	 * 
-	 * @param sensor
-	 * @param interval
-	 * @return
+	 * @param sensor Ultrasonic object
+	 * @param time Total time for polling in milliseconds
+	 * @param interval Interval between polling in milliseconds
+	 * @return Average distance
 	 */
-	public static double avgDistance(Ultrasonic sensor, long interval)
-	{
-		new Thread(new Runnable()
-		{
+	public static double avgDistance(Ultrasonic sensor, long time, long interval) {
+		List<Double> dist = new ArrayList<>();
+		
+		new Thread(new Runnable() {
 			@Override
-			public void run()
-			{
-				while(true)
-				{
+			public void run() {
+				while(true) {
 					try {
+						dist.add(sensor.getRangeInches());
 						sensor.getRangeInches();
-						Thread.sleep(interval);
+						Thread.sleep(time / interval);
 					} catch (InterruptedException e) {
 						e.printStackTrace();
 					}
@@ -46,23 +53,41 @@ public class RobotUtils
 			}
 		}).start();
 		
-		return avgDistance(0);
+		return avgDistance(convertArray(dist));
 	}
 	
 	/**
+	 * Find the average distance from
+	 * an ultrasonic/proximity sensor.
+	 * 
+	 * @param distances Distances from sensor
+	 * @return Average distance
+	 */
+	public static double avgDistance(double... distances) {
+		int total = 0;
+		
+		// Find sum all of the values
+		for (int i = 0; i < distances.length; i++) {
+			total += distances[i];
+		}
+		
+		return total / distances.length;
+	}
+	
+	/**
+	 * Helper method to convert list of
+	 * doubles to an array of doubles
 	 * 
 	 * @param values
 	 * @return
 	 */
-	public static double avgDistance(double... values)
-	{
-		int total = 0;
+	private static double[] convertArray(List<Double> values) {
+		double[] array = new double[values.size()];
 		
-		// Find sum all of the values
-		for (int i = 0; i < values.length; i++) {
-			total += values[i];
+		for (int i = 0; i < array.length; i++) {
+			array[i] = values.get(i);
 		}
 		
-		return total / values.length;
+		return array;
 	}
 }
