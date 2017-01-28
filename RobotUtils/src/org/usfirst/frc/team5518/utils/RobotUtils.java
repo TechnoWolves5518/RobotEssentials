899d1ebfd8624f5ab93ae4bfece2a9a7c50c5934
Hamzah Aslam <haz540@gmail.com>
2017-01-27 23:09:38 -0500

    Added ExecutionService to stop thread after specified time

--- RobotUtils/src/org/usfirst/frc/team5518/utils/RobotUtils.java
+++ RobotUtils/src/org/usfirst/frc/team5518/utils/RobotUtils.java
@@ -2,6 +2,9 @@ package org.usfirst.frc.team5518.utils;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import edu.wpi.first.wpilibj.Ultrasonic;
 
@@ -38,20 +41,27 @@ public class RobotUtils
 	public static double avgDistance(Ultrasonic sensor, long time, long interval) {
 		List<Double> dist = new ArrayList<>();
 		
-		new Thread(new Runnable() {
-			@Override
-			public void run() {
-				while(true) {
-					try {
-						dist.add(sensor.getRangeInches());
-						sensor.getRangeInches();
-						Thread.sleep(time / interval);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
+		ExecutorService executor = Executors.newSingleThreadExecutor();
+		try {
+			executor.submit(
+				new Thread(new Runnable() {
+					@Override
+					public void run() {
+						while(true) {
+							try {
+								dist.add(sensor.getRangeInches());
+								sensor.getRangeInches();
+								Thread.sleep(time / interval);
+							} catch (InterruptedException e) {
+								e.printStackTrace();
+							}
+						}
 					}
-				}
-			}
-		}).start();
+				}))
+			.get(time, TimeUnit.MILLISECONDS);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
 		return avgDistance(convertArray(dist));
 	}
